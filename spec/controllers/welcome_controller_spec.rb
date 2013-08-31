d7e7798ae15a83aa127e344fda6cec041186f9e2
Jason Torres <jason.e.torres@gmail.com>
2013-08-31 08:38:31 +0800

    Added tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe WelcomeController do
 
+  describe '#index' do
+    it 'returns a valid page' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
 end
